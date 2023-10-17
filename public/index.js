11cb61e0e4c97a5e105a59359d27049f4d8fb282
Eduardo Puga <0470hdbutter@gmail.com>
2023-10-16 20:01:10 -0700

    finished work with comments

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+//  i learned how to structure the charts and how to make it populate in the canvas through query selector
 async function main() {
   const timeChartCanvas = document.querySelector("#time-chart");
   const highestPriceChartCanvas = document.querySelector(
@@ -10,7 +11,7 @@ async function main() {
   const { GME, MSFT, DIS, BNTX } = mockData;
 
   const stocks = [GME, MSFT, DIS, BNTX];
-
+  // learned how to address the stock values and how to map them with parseFloat onto a chart
   new Chart(timeChartCanvas.getContext("2d"), {
     type: "line",
     data: {
@@ -23,8 +24,19 @@ async function main() {
       })),
     },
   });
-
+  // setting the first number to be the highest. comparing each number to the first number. updating the highest value
+  function findHighest(values) {
+    let highest = values[0].high;
+    values.forEach((value) => {
+      if (parseFloat(value.high) > highest) {
+        highest = value.high;
+      }
+    });
+    // storing the information
+    return highest;
+  }
   new Chart(highestPriceChartCanvas.getContext("2d"), {
+    // changed the value type to a bar graph
     type: "bar",
     data: {
       labels: stocks.map((stock) => stock.meta.symbol),
@@ -38,6 +50,15 @@ async function main() {
       ],
     },
   });
+  // finding average by adding up all the numbers and then dividing it by how many items there are
+  function calculateAverage(values) {
+    let total = 0;
+    values.forEach((value) => {
+      total += parseFloat(value.high);
+    });
+    // storing information and then dividing by the number of items there are
+    return total / values.length;
+  }
 
   new Chart(averagePriceChartCanvas.getContext("2d"), {
     type: "pie",
@@ -68,24 +89,6 @@ function getColor(stock) {
   if (stock === "BNTX") {
     return "rgba(166, 43, 158, 0.7)";
   }
-}
-
-function findHighest(values) {
-  let highest = values[0].high;
-  values.forEach((value) => {
-    if (parseFloat(value.high) > highest) {
-      highest = value.high;
-    }
-  });
-  return highest;
-}
-
-function calculateAverage(values) {
-  let total = 0
-  values.forEach(value => {
-    total += parseFloat(value.high)
-  })
-  return total/values.length
 }
 
 main();
